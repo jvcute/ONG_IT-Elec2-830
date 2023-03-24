a3f778539358cabadb6eeb381c269d0466fb34fc
John Victor Ong <mijakolgaming@gmail.com>
2023-03-24 11:51:10 +0800

    fragment and pizza activity

--- app/src/main/java/com/example/jvcute/ListActivity.java
+++ app/src/main/java/com/example/jvcute/ListActivity.java
@@ -3,12 +3,26 @@ package com.example.jvcute;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ListActivity extends AppCompatActivity {
 
+    ListView listView;
+    TextView textView;
+
+    String[] listItem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        listView=(ListView)findViewById(R.id.listView);
+        listItem = getResources().getStringArray(R.array.array_technology);
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.my_list, R.id.textView, listItem);
+        listView.setAdapter(adapter);
+
     }
 }
